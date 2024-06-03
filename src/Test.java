8b68a1cbf6c8021f64a70364d69d483f6a85da35
ogasawara-di <vawui00997@yahoo.co.jp>
2024-06-03 21:11:54 +0900

    Javaデータベース接続（演習②）修正2

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,11 @@ public class Test {
 	public static void main(String[] args) {
 		TestUserDAO dao = new TestUserDAO();
 		dao.select("taro", "123");
+		dao.selectAll();
+		dao.selectByName("taro");
+		dao.selectByPassword("123");
+		dao.updateUserNameByUserName("taro", "saburo");
+		dao.insert(4, "shiro", "012");
 	}
 }
+//※実行をするとshiroのデータは実行した分登録される
